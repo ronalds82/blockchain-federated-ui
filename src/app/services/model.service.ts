05eb73c9169f6a7690fbcb192fd65ae4c27311c0
AndrisWillow <andris.vitols@gmail.com>
2025-01-10 12:33:34 +0200

    Removed old code

--- src/app/services/model.service.ts
+++ src/app/services/model.service.ts
@@ -12,10 +12,6 @@ import { ModelResponse } from '../models/model-response.model';
 export class ModelService {
   constructor(private http: HttpClient) {}
 
-  getModels(): Observable<GlobalModel[]> {
-    return this.http.get<GlobalModel[]>(`${baseUrl}/models`);
-  }
-
   uploadModel(file: File | null, hospitalId: number): Observable<CustomResponse> {
     return this.http.post<CustomResponse>(`${baseUrl}/upload`, { file, hospitalId });
   }
